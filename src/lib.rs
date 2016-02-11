2f01ff25c29c417ecf06f376c4818490fe053730
darkstalker <slayerbeast@gmail.com>
2016-02-11 18:55:41 -0300

    added opt_slice for converting an &Option<A> into slice

--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,30 @@ pub fn mut_ref_slice<A>(s: &mut A) -> &mut [A] {
     }
 }
 
+/// Converts a reference to Option<A> into a slice of length 0 or 1 (without copying).
+pub fn opt_slice<A>(opt: &Option<A>) -> &[A]
+{
+    match *opt {
+        Some(ref val) => ref_slice(val),
+        None => &[],
+    }
+}
+
+/// Converts a reference to Option<A> into a slice of length 0 or 1 (without copying).
+pub fn mut_opt_slice<A>(opt: &mut Option<A>) -> &mut [A]
+{
+    match *opt {
+        Some(ref mut val) => mut_ref_slice(val),
+        None => &mut [],
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::ref_slice;
     use super::mut_ref_slice;
+    use super::opt_slice;
+    use super::mut_opt_slice;
 
     #[test]
     fn check() {
@@ -35,5 +55,33 @@ mod tests {
         let result: &mut [i32] = &mut [5];
 
         assert_eq!(result, xs);
+    }
+
+    #[test]
+    fn check_opt() {
+        let x = &Some(42);
+        let n = &None;
+        let xs = opt_slice(x);
+        let ns = opt_slice(n);
+
+        let result_x: &[i32] = &[42];
+        let result_n: &[i32] = &[];
+
+        assert_eq!(result_x, xs);
+        assert_eq!(result_n, ns);
+    }
+
+    #[test]
+    fn check_opt_mut() {
+        let x = &mut Some(42);
+        let n = &mut None;
+        let xs = mut_opt_slice(x);
+        let ns = mut_opt_slice(n);
+
+        let result_x: &[i32] = &mut [42];
+        let result_n: &[i32] = &mut [];
+
+        assert_eq!(result_x, xs);
+        assert_eq!(result_n, ns);
     }
 }
